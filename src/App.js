6278508aff55f61e5b5010e71705d24940b24fc6
quangdang87 <vinhquangdang1@gmail.com>
2021-04-02 14:47:47 -0700

    passed tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {LOGIN_REQUEST, LOGOUT} from "./actions/actionTypes";
+import "./App.css";
+import logo from "./logo.svg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>
+          <button onClick={this.props.login}>Login</button>
+          <button onClick={this.props.logout}>Logout</button>
+        </div>
+        <p>status: {this.props.status}</p>
+        <p>token: {this.props.token || ""}</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state, ownProps) => {
+  return {
+    token: state.login.token,
+    status: state.login.status,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    login: () =>
+      dispatch({type: LOGIN_REQUEST, user: "NoriSte", password: "password"}),
+    logout: () => dispatch({type: LOGOUT}),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,1 +1,36 @@
 // add sagas for this login page according to the actions triggered.
+// import {loggedIn} from "../actions/loginActions";
+import actionTypes from "../actions/actionTypes";
+import {LOGIN_SUCCESS} from "../actions/actionTypes";
+import {call, cancel, cancelled, fork, put, take} from "redux-saga/effects";
+import {loggedIn} from "../actions/loginActions";
+
+export function* loginFlow() {
+  while (1) {
+    let credentials = yield take("LOGIN_REQUEST");
+    let task = yield fork(authorize, credentials.user, credentials.password);
+    let authorizeForkMock = yield take(["LOGOUT", "LOGIN_ERROR"]);
+    if (authorizeForkMock.type === "LOGOUT") yield cancel(task);
+
+    yield put({type: "DELETE_TOKEN"});
+  }
+}
+
+export function* logActions() {}
+export function* fakeAuthorize(user, password) {
+  yield loggedIn();
+}
+export function* authorize(user, password) {
+  while (1) {
+    try {
+      let token = yield call(fakeAuthorize, user, password);
+      yield put({type: "LOGIN_SUCCESS"});
+      yield put({type: "SAVE_TOKEN", token});
+      let check = yield cancelled();
+      console.log("check: ", check);
+      if (check) yield put({type: "LOGIN_CANCELLED"});
+    } catch (error) {
+      yield put({type: "LOGIN_ERROR", error});
+    }
+  }
+}
